4764a3bff09d30c18ea4b8798948d6c7195a82ad
codingvaleria <codingvaleria@gmail.com>
2022-12-06 15:30:02 +0300

    update Auth file and css

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import Login from "./Login";
 import SignUp from "./SignUp";
 
-export default function Auth() {
+export default function Auth({ setShowNavBar }) {
+  // hides the navigation bar while on this page
+  setShowNavBar(false);
+  // switches between login && resgistration
   const [login, setLogin] = useState(true);
 
+  // changing state
   function handleClick(e) {
     e.preventDefault();
     setLogin(!login);
