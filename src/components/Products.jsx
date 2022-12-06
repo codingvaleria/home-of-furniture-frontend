f28821b782a65a66185437504f74fae22d715fd7
codingvaleria <codingvaleria@gmail.com>
2022-12-06 17:12:38 +0300

    refactor products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,79 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Products.css";
+import Loading from "./Loading";
+import Item from "./Item";
 
 export default function Products({ setShowNavBar }) {
   setShowNavBar(true);
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  console.log(products);
 
   return (
     <div className="products-container">
-      <div className="product">
-        <div className="itemx">
-          <img
-            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
-            alt=""
-          />
-          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
-          <p>
-            Kshs. <span>259995</span>
-          </p>
-          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
-          <div className="action-price">
-            <button className="bt1">Update Item</button>
-            <button className="bt2">Delete Item</button>
-          </div>
-        </div>
-      </div>
-      <div className="product">
-        <div className="itemx">
-          <img
-            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
-            alt=""
-          />
-          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
-          <p>
-            Kshs. <span>259995</span>
-          </p>
-          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
-          <div className="action-price">
-            <button className="bt1">Update Item</button>
-            <button className="bt2">Delete Item</button>
-          </div>
-        </div>
-      </div>
-      <div className="product">
-        <div className="itemx">
-          <img
-            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
-            alt=""
-          />
-          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
-          <p>
-            Kshs. <span>259995</span>
-          </p>
-          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
-          <div className="action-price">
-            <button className="bt1">Update Item</button>
-            <button className="bt2">Delete Item</button>
-          </div>
-        </div>
-      </div>
-      <div className="product">
-        <div className="itemx">
-          <img
-            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
-            alt=""
-          />
-          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
-          <p>
-            Kshs. <span>259995</span>
-          </p>
-          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
-          <div className="action-price">
-            <button className="bt1">Update Item</button>
-            <button className="bt2">Delete Item</button>
-          </div>
-        </div>
-      </div>
+      {isLoading ? <Loading /> : <Item products={products} />}
     </div>
   );
 }
