3497504913e237df2b9d4aa0bd8eb4ac67773ef3
codingvaleria <codingvaleria@gmail.com>
2022-12-06 18:01:38 +0300

    add filter functionality

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "../styles/Products.css";
 import Loading from "./Loading";
 import Item from "./Item";
+import Filter from "./Filter";
 
 export default function Products({ user }) {
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -12,15 +14,47 @@ export default function Products({ user }) {
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
+        setFilter(data);
         setIsLoading(false);
       });
   }, []);
 
   console.log(products);
+  const filterProducts = (category) => {
+    const updatedList = products.filter(
+      (product) => product.category === category
+    );
+    setFilter(updatedList);
+  };
+
+  // search state
+  const [query, setQuery] = useState("");
+
+  const keys = ["name", "description"];
+
+  function search(data) {
+    return data.filter((data) =>
+      keys.some((key) => data[key].toLowerCase().includes(query.toLowerCase()))
+    );
+  }
+
+  const infomations = filter.filter((data) =>
+    keys.some((key) => data[key].toLowerCase().includes(query.toLowerCase()))
+  );
 
   return (
     <div className="products-container">
-      {isLoading ? <Loading /> : <Item products={products} user={user} />}
+      <Filter
+        input={setQuery}
+        setFilter={setFilter}
+        filterProducts={filterProducts}
+        products={products}
+      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Item products={filter} user={user} search={search} />
+      )}
     </div>
   );
 }
