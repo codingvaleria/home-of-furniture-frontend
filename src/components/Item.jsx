8ea749588a10994b2bb37665b609122a3f88bd4a
codingvaleria <codingvaleria@gmail.com>
2022-12-06 17:35:35 +0300

    update item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Products.css";
 
 export default function Item({ products }) {
+  const navigate = useNavigate();
+
   return (
     <div>
       {products.map((product) => (
         <div className="product" key={product.id}>
           <div className="itemx">
-            <img
-              src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
-              alt=""
-            />
+            <img src={product.image} alt="" />
             <h3>{product.name}</h3>
             <p>
               Kshs. <span>{product.price}</span>
             </p>
-            <p>{product.description.substring(0, 50)}...</p>
+            <p>{product.description.substring(0, 40)}...</p>
             <div className="action-price">
-              <button className="bt1">Update Item</button>
+              <button
+                className="bt1"
+                onClick={() => navigate(`/${product.id}`)}
+              >
+                Update Item
+              </button>
               <button className="bt2">Delete Item</button>
             </div>
           </div>
