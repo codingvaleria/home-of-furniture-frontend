293be145d5763477f26d332b2ec94ac2f55e295a
codingvaleria <codingvaleria@gmail.com>
2022-12-06 15:22:09 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProduct from "./AddProduct";
+import Products from "./Products";
+import NavBar from "./NavBar";
+import Auth from "./Auth";
 import "./App.css";
 
 function App() {
+  const [showNavBar, setShowNavBar] = useState(true);
   return (
     <div className="App">
-      <h1> Home of Furnitures </h1>
+      <Router>
+        {showNavBar && <NavBar />}
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/addproduct" element={<AddProduct />} />
+          <Route path="products/:id" element={<Products />} />
+          <Route path="auth" element={<Auth setShowNavBar={setShowNavBar} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
