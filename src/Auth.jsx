3b9eab35a8d1850c6a63faf51b461763639c150c
codingvaleria <codingvaleria@gmail.com>
2022-12-06 15:37:32 +0300

    add hide navbar functionality

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -3,17 +3,15 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 
 export default function Auth({ setShowNavBar }) {
-  // hides the navigation bar while on this page
+  // hide navigation bar for this component
   setShowNavBar(false);
-  // switches between login && resgistration
+
   const [login, setLogin] = useState(true);
 
-  // changing state
   function handleClick(e) {
     e.preventDefault();
     setLogin(!login);
   }
-
   return (
     <div>
       {login ? (
