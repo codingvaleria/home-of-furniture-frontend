8931cce8b6a0d1dc2f8b9f4b268eaed10c1c68e8
codingvaleria <codingvaleria@gmail.com>
2022-12-06 14:52:02 +0300

    add Product component && styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,35 +1,96 @@
 import React from "react";
-import "./styles/Product.css";
+import "./styles/Products.css";
 
-export default function Product() {
+export default function Products() {
   return (
-    <div className="item">
-      <div className="product-item">
-        <div className="product-details">
+    <div className="products-container">
+      <div className="product">
+        <div>
           <img
             src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
             alt=""
           />
-        </div>
-        <div className="product-details">
-          <h1>Edwin Aire Leather Recliner Sofa Set</h1>
-          <h2>Category</h2>
-          <h2>Price</h2>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius,
-            fugiat consequuntur culpa tenetur, magnam dolorum repudiandae quae
-            quisquam dolore ab, ut delectus tempore quis iste aliquam officia
-            molestiae doloremque laboriosam.
-          </p>
-          <div className="action-btns">
-            <button>Add To Cart</button>
-            <button>Go To Cart</button>
-          </div>
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
         </div>
       </div>
-      <div className="priviledges">
-        <button className="edit-btn">Edit Item</button>
-        <button className="delete-btn">Delete Item</button>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
+      </div>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
+      </div>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
+      </div>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
+      </div>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
+      </div>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
+      </div>
+      <div className="product">
+        <div>
+          <img
+            src="https://elegance.co.ke/pub/media/catalog/product/cache/b7c3a6b28eb4432f8bd6c315e4890c68/6/0/6033_1.jpg"
+            alt=""
+          />
+          <h3>Edwin Aire Leather Recliner Sofa Set</h3>
+          <p>259995</p>
+          <p>Loreum suscipit nemo debitis ipsum numquam minima.</p>
+        </div>
       </div>
     </div>
   );
