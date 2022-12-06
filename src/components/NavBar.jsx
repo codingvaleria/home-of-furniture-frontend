9bf1c146448f4ded91b7b30f22b10ac19e20576f
codingvaleria <codingvaleria@gmail.com>
2022-12-06 18:03:31 +0300

    add log out functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,32 @@ import React from "react";
 import "../styles/NavBar.css";
 import { Link } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({ setUser, user }) {
+  function handleLogoutClick() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
   return (
-    <div>
-      <div className="navbar">
-        <nav>
-          <input type="checkbox" name="" id="check" />
-          <label htmlFor="check" className="checkbtn">
-            <i className="fas fa-bars"></i>
-          </label>
-          <label className="logo">Funiture Shop</label>
-          <div className="nav-menu">
-            <Link to="/">Home</Link>
-          </div>
-        </nav>
-      </div>
+    <div className="navbar">
+      <nav>
+        <input type="checkbox" name="" id="check" />
+        <label htmlFor="check" className="checkbtn">
+          <i className="fas fa-bars"></i>
+        </label>
+        <label className="logo">Funiture Shop</label>
+        <div className="nav-menu">
+          {user && (
+            <>
+              <Link to="/">Home</Link>
+              <Link to="/addproduct">Add Product</Link>
+              <Link onClick={handleLogoutClick}>Logout</Link>
+            </>
+          )}
+        </div>
+      </nav>
     </div>
   );
 }
