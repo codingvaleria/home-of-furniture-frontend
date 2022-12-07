90cc9cea9ce6faadcd0d86cb3dc3133ecb8d25e1
codingvaleria <codingvaleria@gmail.com>
2022-12-08 00:43:16 +0300

    add product detail component

---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "./Loading";
+import "../styles/Product.css";
+
+export default function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProduct = async () => {
+    fetch(`/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setProduct(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchProduct();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const Product = () => {
+    return (
+      <div className="single-item">
+        <div>
+          <img src={product.image} alt={product.name} />
+        </div>
+        <div className="info">
+          <h1>{product.name}</h1>
+          <h2>{product.category}</h2>
+          <h3>Kshs. {product.price}</h3>
+          <p>{product.description}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return <div>{loading ? <Loading /> : <Product />}</div>;
+}
